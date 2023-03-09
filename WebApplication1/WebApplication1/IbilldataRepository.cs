8081d91a5f4c14fadd48e1afcc6b6540fe45b7e8
M SUMUKHI SRI SAI <mathapathisumukhi@gmail.com>
2023-03-09 12:09:59 -0500

    commit

---
+++ WebApplication1/WebApplication1/IbilldataRepository.cs
@@ -0,0 +1,23 @@
+﻿using billdataRestAPIMySQL.Models;
+namespace billdataRestAPIMySQL.Interfaces
+{
+    public interface IbilldataRepository
+    {   /// <summary>
+        /// Interface Repository for declaring all the methods called in Repository
+        /// </summary>
+        /// <returns> called function</returns>
+        ICollection<billdata> getItems();
+        billdata getItem(int id);
+        bool addItem(billdata item);
+
+        bool editItem(billdata item);
+
+        bool deleteItem(int id);
+
+        billdata MaxElec();
+
+        billdata MinGas();
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/billdata.cs
@@ -0,0 +1,31 @@
+﻿namespace billdataRestAPIMySQL.Models
+{
+    public class billdata
+    {   /// <summary>
+        /// Structure of the Data. It takes the following parameters
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="electricityBill"></param>
+        /// <param name="waterBill"></param>
+        /// <param name="gasBill"></param>
+
+        public int Id { get; set; }
+
+        public string firstNames { get; set; }
+
+        public string lastNames { get; set; }
+
+        public double electricity { get; set; }
+
+        public double water { get; set; }
+
+        public double gas { get; set; }
+
+        // {get;set;} method is an automatic property provided by c#.
+        // The get methid is used to return the value of variable
+        // The set method is used to set value to the variable
+
+    }
+}
