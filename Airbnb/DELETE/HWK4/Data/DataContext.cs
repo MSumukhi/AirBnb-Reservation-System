a26391158198d92d224903a3d3544d45534767e7
kattr01 <122712866+kattr01@users.noreply.github.com>
2023-03-11 22:13:58 -0500

    delete

---
+++ Airbnb/DELETE/HWK4/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using HWK4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HWK4.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Airbnb> Airbnb { get; set; }
+    }
+}
+ 
