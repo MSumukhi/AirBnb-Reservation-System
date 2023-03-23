80aaead304e90c3a0fa50caad1adcd6233fe72e6
MSumukhi <122749850+MSumukhi@users.noreply.github.com>
2023-03-23 12:50:33 -0400

    Update DataContext.cs

--- Airbnb/HWK4/Data/DataContext.cs
+++ Airbnb/HWK4/Data/DataContext.cs
@@ -1,13 +1,13 @@
-﻿using HWK4.Models;
+using Airbnb.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace HWK4.Data
+namespace Airbnb.Data
 {
-    public class DataContext: DbContext
+    using Airbnb.Models;
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Airbnb> Airbnb { get; set; }
     }
 }
- 
