1e23249fca83514d954e7c58e03d086b72cfefe0
MSumukhi <122749850+MSumukhi@users.noreply.github.com>
2023-03-25 18:44:20 -0400

    Update IAirbnbRepository.cs
    added two new methods of getting reviews and adding a review

--- Airbnb/HWK4/Interfaces/IAirbnbRepository.cs
+++ Airbnb/HWK4/Interfaces/IAirbnbRepository.cs
@@ -3,6 +3,7 @@ using Airbnb.Models;
 namespace Airbnb.Interfaces
 {
     using Airbnb.Models;
+    using Airbnb_review.Models;
     /// <summary>
     /// The interface provides the declarations for the methods used
     /// </summary>
@@ -22,6 +23,10 @@ namespace Airbnb.Interfaces
 
         int getMax();
         ICollection<Airbnb> Availability();
+
+        ICollection<Airbnb_review> GetReviews();
+
+        bool AddReview(Airbnb_review review);
 
         bool Save();
     }
