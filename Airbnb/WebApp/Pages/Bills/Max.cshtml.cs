e48fb41541d92cd7e29d58fb0feae70506456abc
MSumukhi <122749850+MSumukhi@users.noreply.github.com>
2023-04-04 17:02:54 -0400

    commit

--- Airbnb/WebApp/Pages/Bills/Max.cshtml.cs
+++ Airbnb/WebApp/Pages/Bills/Max.cshtml.cs
@@ -2,29 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 
-///<summary>
-///In Max, the data analysis is done by obtaining the Maximum amount of the bill list and is displayed in the 
-///front-end
-///</summary>
-namespace WebApp.Pages.Bills
+namespace AirbnbWebApp.Pages.bill
 {
     public class MaxModel : PageModel
     {
-        public int max = new();
+        public int max;
+        /// <summary>
+        /// Retreiving the details of airbnb that is highly priced
+        /// </summary>
         public async void OnGet()
         {
             var client = new HttpClient();
-            
-            var responseTask = client.GetAsync("https://localhost:7135/Bill/Analysis-GetHighestAmount");
+
+            var responseTask = client.GetAsync("https://localhost:7001/Airbnb/Analysis-GetHighestAmount");
             responseTask.Wait();
             var result = responseTask.Result;
             if (result.IsSuccessStatusCode)
             {
                 var content = await result.Content.ReadAsStringAsync();
-               
+
 
                 max = JsonConvert.DeserializeObject<int>(content);
-                
+
             }
         }
     }
