64659a3b339b1f854e3dbf1264e8bd4b05f0ecd2
MSumukhi <122749850+MSumukhi@users.noreply.github.com>
2023-04-04 17:00:49 -0400

    updated

--- Airbnb/WebApp/Pages/Bills/Index.cshtml.cs
+++ Airbnb/WebApp/Pages/Bills/Index.cshtml.cs
@@ -1,26 +1,32 @@
-using HWK4.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Airbnb.Interfaces;
+using Microsoft.OpenApi.Models;
 
-namespace WebApp.Pages.Bills
+
+namespace AirbnbWebApp.Pages.bill
 {
+    using Airbnb.Models;
     public class IndexModel : PageModel
     {
+        public List<Airbnb> bill = new();
 
-        public List<Bill> Bills = new();
+        /// <summary>
+        /// Retrieving Airbnb list from the database
+        /// </summary>
         public async void OnGet()
         {
-           using (var client=new HttpClient())
+            using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:5073");
-                var responseTask = client.GetAsync("Bill");
+                client.BaseAddress = new Uri("http://localhost:5289");
+                var responseTask = client.GetAsync("Airbnb");
                 responseTask.Wait();
                 var result = responseTask.Result;
                 if (result.IsSuccessStatusCode)
                 {
-                    var readTask=await result.Content.ReadAsStringAsync();
-                    Bills = JsonConvert.DeserializeObject<List<Bill>>(readTask);
+                    var readTask = await result.Content.ReadAsStringAsync();
+                    bill = JsonConvert.DeserializeObject<List<Airbnb>>(readTask);
                 }
             }
         }
