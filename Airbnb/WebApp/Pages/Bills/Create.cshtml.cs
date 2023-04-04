a8a56ec5919e3224672e171edeb4e700e0797d4c
MSumukhi <122749850+MSumukhi@users.noreply.github.com>
2023-04-04 16:55:44 -0400

    Documented and changes

--- Airbnb/WebApp/Pages/Bills/Create.cshtml.cs
+++ Airbnb/WebApp/Pages/Bills/Create.cshtml.cs
@@ -1,41 +1,66 @@
-using HWK4.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using static System.Net.WebRequestMethods;
+using System.Xml.Linq;
 using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data;
 
-///<summary>
-///Here in the Create file, we are creating a new item of bill which gets all the attributes
-///from the user from the front-emd and passes it to the controller which then adds it to the database
-///</summary>
-namespace WebApp.Pages.Bills
+
+namespace AirbnbWebApp.Pages.bill
 {
+    using Airbnb.Models;
+    using Airbnb.Data;
+    using Airbnb.Interfaces;
+    using Airbnb.Repositories;
+    using Airbnb.Controllers;
     public class CreateModel : PageModel
     {
-        public HWK4.Models.Bill bill = new();
+
+        public Airbnb bill = new();
         public string errorMessage = "";
         public string successMessage = "";
 
+        /// <summary>
+        /// Posting a new Airbnb with each attribute of the airbnb structure
+        /// </summary>
         public async void OnPost()
         {
             bill.Id = int.Parse(Request.Form["Id"]);
-            bill.Category = Request.Form["category"];
-            bill.Amount = int.Parse(Request.Form["amount"]);
-            bill.Date = Request.Form["date"];
-            if (bill.Category.Length == 0)
+            bill.name = Request.Form["name"];
+            bill.host_id = Request.Form["host_id"];
+            bill.host_name = Request.Form["host_name"];
+            bill.neighbourhood_group = Request.Form["neighbourhood_group"];
+            bill.neighbourhood = Request.Form["neighbourhood"];
+            bill.latitude = double.Parse(Request.Form["latitude"]);
+            bill.longitude = double.Parse(Request.Form["longitude"]);
+            bill.roomtype = Request.Form["roomtype"];
+            bill.price = int.Parse(Request.Form["price"]);
+            bill.minimum_nights = int.Parse(Request.Form["minimum_nights"]);
+            bill.number_of_reviews = int.Parse(Request.Form["number_of_reviews"]);
+            bill.reviews_per_month = double.Parse(Request.Form["reviews_per_month"]);
+            bill.availability_365 = Request.Form["availability_365"];
+            bill.reviews = Request.Form["reviews"];
+            bill.max_people = int.Parse(Request.Form["max_people"]);
+            bill.children_amenities = Request.Form["children_amenities"];
+
+            //checking if name is empty and notifying that it is required
+            if (bill.name.Length == 0)
             {
-                errorMessage = "Category is required";
+                errorMessage = "FirstName is required";
             }
             else
             {
                 var opt = new JsonSerializerOptions() { WriteIndented = true };
-                string json = System.Text.Json.JsonSerializer.Serialize<Bill>(bill, opt);
+                string json = System.Text.Json.JsonSerializer.Serialize<Airbnb>(bill, opt);
 
                 using (var client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://localhost:5073");
+                    client.BaseAddress = new Uri("http://localhost:5289");
 
                     var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-                    var result = await client.PostAsync("Bill", content);
+                    var result = await client.PostAsync("Airbnb", content);
                     string resultContent = await result.Content.ReadAsStringAsync();
                     Console.WriteLine(resultContent);
 
@@ -50,7 +75,7 @@ namespace WebApp.Pages.Bills
                 }
             }
 
-           
+
         }
     }
 }
