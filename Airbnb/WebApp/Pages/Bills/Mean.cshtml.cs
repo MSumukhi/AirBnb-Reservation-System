21147e2797f771424bbe3d81f6692f82d373dc4f
MSumukhi <122749850+MSumukhi@users.noreply.github.com>
2023-04-04 17:01:55 -0400

    Documented

--- Airbnb/WebApp/Pages/Bills/Mean.cshtml.cs
+++ Airbnb/WebApp/Pages/Bills/Mean.cshtml.cs
@@ -1,21 +1,20 @@
-using HWK4.Models;
+using Airbnb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 
-///<summary>
-///In Mean , the average value of the total bill amount is calculated and it displayed in the front-end.
-///The value is obtained by calling the GetMean method in the controller.
-///</summary>
-namespace WebApp.Pages.Bills
+namespace AirbnbWebApp.Pages.bill
 {
     public class MeanModel : PageModel
     {
-        public int mean=new();
+        public int mean = new();
+        /// <summary>
+        /// Retreiving the everage cost of Airbnbs listed from the database
+        /// </summary>
         public async void OnGet()
         {
             var client = new HttpClient();
-            var responseTask = client.GetAsync("https://localhost:7135/Bill/Analysis-GetMean");
+            var responseTask = client.GetAsync("https://localhost:7001/Airbnb/Analysis-GetMean");
             responseTask.Wait();
             var result = responseTask.Result;
             if (result.IsSuccessStatusCode)
@@ -23,10 +22,10 @@ namespace WebApp.Pages.Bills
                 var content = await result.Content.ReadAsStringAsync();
                 //var data = JsonConvert.DeserializeObject<dynamic>(content);
 
-               mean = JsonConvert.DeserializeObject<int>(content);
+                mean = JsonConvert.DeserializeObject<int>(content);
                 // Use the response data here
                 //var message = data.Message;
             }
         }
-}
+    }
 }
